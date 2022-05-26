79c7b350ce142bc09b62db3cba5d047a2f182e1d
pablomdd <pablo.mdd.007@gmail.com>
2022-05-26 00:00:33 -0500

    add test imgs, fix sim function

--- tf_detector/sacMatlabApi.m
+++ tf_detector/sacMatlabApi.m
@@ -1,5 +1,6 @@
-function [sacOutputSpeed] = sacMatlabApi(rpmSpeed)
+function [sacOutputSpeedRpm] = sacMatlabApi(rpmSpeed)
 %% SETUP
+clear all;
 pyversion
 sacPath = fileparts(which('main.py'))
 
@@ -7,21 +8,31 @@ if count(py.sys.path,sacPath) == 0
     insert(py.sys.path,int32(0),sacPath);
 end
 %% Run python script
-img_route = "test_imgs/1.png"
-sacSpeed = 15 %km/h
-pyOut = py.main.predict(pyargs('img_route', img_route, 'speed', sacSpeed));
+img_route = "test_imgs/20.png"
+sacSpeed = 20 %km/h
+pyOut = py.main.predict( ...
+    pyargs('img_route', img_route, ...
+            'speed', sacSpeed));
 action = pyOut{1};
 disp(action(:))
 
-sacOutputSpeed = 0;
+%% Processing Python output to simulink
 
+% Output speed placeholder
+sacOutputSpeedRpm = 0;
 if action == "down15"
     % 15 km/h
-    sacOutputSpeed = 368.59; %rpm
+    sacOutputSpeedRpm = 368.59; %rpm
 elseif action == "down10"
     % 10 km/h
-    sacOutputSpeed = 275.42; %rpm
+    sacOutputSpeedRpm = 275.42; %rpm
 else 
     % 5 km/h
-    sacOutputSpeed = 122.86; %rpm
+    sacOutputSpeedRpm = 122.86; %rpm
 end
+% Save rpm speed programatically in the workspace
+assignin('base','sacOutputSpeedRpm', sacOutputSpeedRpm)
+%% Call simulink BLDC sacOutputSpeedRpmsimulation.
+sim("BLDC_PWM_control.slx")
+
+return
