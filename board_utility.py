14432107ef1b1613c5e11371db34727e47caac05
Pablo D <pablo.mdd.007@gmail.com>
2022-11-12 02:26:45 -0600

    add board utility class

--- board_utility.py
+++ board_utility.py
@@ -7,31 +7,51 @@ DEFAULT_PORT = "/dev/ttyACM0"
 # DEFAULT_PORT = "/dev/ttyUSB0"
 
 DEFAULT_BAUD_RATE = 115200
+DEFAULT_TIMEOUT = 0.1
 
 class Board:
-    def __init__(self, port=DEFAULT_PORT, baud_rate=DEFAULT_BAUD_RATE) -> None:
+    def __init__(self, port=DEFAULT_PORT, baud_rate=DEFAULT_BAUD_RATE, timeout=DEFAULT_TIMEOUT, debug_mode=True) -> None:
         self.port = port
         self.baud_rate = baud_rate
+        self.timeout = timeout
+        self.debug_mode = debug_mode
 
         try:
-            self.board = serial.Serial(port=self.port, baudrate=self.baud_rate)
+            self.board = serial.Serial(port=self.port, baudrate=self.baud_rate, timeout=self.timeout)
         except:
             raise Exception("Cannot initialize board communication")
 
     def write(self, data:str) -> None:
+        if self.debug_mode:
+            print("writing")
         self.board.write(bytes(data, 'utf-8'))
     
     def read(self) -> str:
-        return self.board.readline().decode('utf-8')
+        try:
+            if self.debug_mode:
+                print("reading")
+            reading = self.board.readline().decode('utf-8')
+            if reading == "":
+                reading = "No reading"
+
+        except:
+            # raise Exception("Error reading board.")
+            print("Error reading board.")
+            return -1
+        return reading
 
 if __name__ == '__main__':
+    print("initializing board")
     board = Board()
-    time.sleep(1)
-    board.write("1")
-    print(board.read())
 
-    time.sleep(1)
-    board.write("3")
-    print(board.read())
+    # Note that board needs about 2 seconds to start writing/reading operations properly
+    time.sleep(2)
+
+    for i in range(-2, 8):
+        board.write(str(i))
+        time.sleep(0.001)
+        action = board.read()
+        print(action)
+        time.sleep(0.1)
 
     
